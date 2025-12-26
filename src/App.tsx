cfc65655039cfb6805865609f0fab085088447aa
Jagdishtripathy <jagadishtripathy144@gmail.com>
2025-12-26 13:26:49 +0530

    fix: react router basename for subdirectory deployment

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { ShortcutsPage } from "@/pages/Shortcuts";
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <ScrollToTop />
       <Layout>
         <Routes>
